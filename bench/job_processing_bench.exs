a8e5edfcb7e105ce4669bc73a4f000ce78252e42
Mitchell Henke <mitchh23@gmail.com>
2016-04-29 15:07:13 -0500

    benchmark

---
+++ bench/job_processing_bench.exs
@@ -0,0 +1,31 @@
+defmodule LastWorker do
+  def perform() do
+    send(:benchmarking_process, :done)
+  end
+end
+
+defmodule ExampleWorker do
+  def perform() do
+  end
+end
+
+defmodule Verk.JobProcessingBench do
+  { :ok, _ } = Application.ensure_all_started(:tzdata)
+
+  Verk.Supervisor.start_link
+  Verk.Queue.clear("benchmark")
+  Process.register(self, :benchmarking_process)
+  Enum.each(1..100000, fn(_) ->
+    Verk.enqueue %Verk.Job{queue: :benchmark, class: "ExampleWorker", args: []}
+  end)
+
+  :timer.sleep(1000)
+  Verk.enqueue %Verk.Job{queue: :benchmark, class: "LastWorker", args: []}
+
+  start = :erlang.monotonic_time(:micro_seconds)
+  Verk.add_queue(:benchmark, 200)
+
+  receive do
+    :done -> IO.puts "#{(:erlang.monotonic_time(:micro_seconds) - start)/1000.0} ms"
+  end
+end
