4006e366a599f5429cb7b1c49938dc01d23da9e9
Mitchell Henke <mitchh23@gmail.com>
2016-04-29 15:07:13 -0500

    benchmark

--- config/config.exs
+++ config/config.exs
@@ -11,3 +11,8 @@ if Mix.env == :test do
   config :verk, queues: [{:default, 1}], redis_url: "redis://127.0.0.1:6379/1"
 end
 
+if Mix.env == :prod do
+  config :logger, backends: [] # Silent logging for tests
+  config :tzdata, :autoupdate, :disabled
+  config :verk, queues: [], redis_url: "redis://127.0.0.1:6379/2", workers_manager_timeout: 0
+end
